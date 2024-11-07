8cc943752704ab6f7749a831e0b90791981f06d2
Test <Test@gmail.com>
2024-11-07 14:38:25 +0800

    fix ui

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,36 +1,38 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
-
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = {
-	theme = "tokyonight",
+M.base46 = {
+  theme = "tokyonight",
 
   transparency = true,
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
+
+}
+M.ui = {
   statusline = {
-      theme = "default", -- 样式选择：default/vscode/vscode_colored/minimal
-      separator_style = "block", -- 分隔符样式选择：default/round/block/arrow
-      order = nil,
-      modules = nil,
-   },
+    theme = "default",         -- 样式选择：default/vscode/vscode_colored/minimal
+    separator_style = "block", -- 分隔符样式选择：default/round/block/arrow
+    order = nil,
+    modules = nil,
+  },
 }
 
 M.term = {
-    winopts = { winhl = "Normal:term,WinSeparator:WinSeparator" },
-    sizes = { sp = 0.3, vsp = 0.2, ["bo sp"] = 0.3, ["bo vsp"] = 0.2 },
-    float = {
-      relative = "editor",
-      row = 0.3,
-      col = 0.25,
-      width = 0.5,
-      height = 0.4,
-      border = "single",
-    },
+  winopts = { winhl = "Normal:term,WinSeparator:WinSeparator" },
+  sizes = { sp = 0.3, vsp = 0.2, ["bo sp"] = 0.3, ["bo vsp"] = 0.2 },
+  float = {
+    relative = "editor",
+    row = 0.3,
+    col = 0.25,
+    width = 0.5,
+    height = 0.4,
+    border = "single",
+  },
 }
 
 return M
---
+++ lua/plugins/ui.lua
@@ -0,0 +1,20 @@
+return {
+  "nvim-lua/plenary.nvim",
+
+  {
+    "nvchad/base46",
+    build = function()
+      require("base46").load_all_highlights()
+    end,
+  },
+
+  {
+    "nvchad/ui",
+    lazy = false,
+    config = function()
+      require "nvchad"
+    end,
+  },
+
+  "nvzone/volt",
+}
